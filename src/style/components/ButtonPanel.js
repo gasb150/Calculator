d77dc5eb119117078c84096173ff61b27f0ddc6f
tavo <gasb_150@hotmail.com>
2021-05-07 09:19:39 -0500

    Add Button component

---
+++ src/style/components/ButtonPanel.js
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react'
+
+
+const ButtonPanel = () =>{ 
+  return(
+    <Fragment>
+    <div>
+      AC, +/-, %, /
+    </div>
+    <div>
+      7, 8, 9, X
+    </div>
+    <div>
+      4, 5, 6, -
+    </div>
+    <div>
+      1, 2, 3, +
+    </div>
+    <div>
+      0, ., =
+    </div>
+    </Fragment>
+  )
+}
+
+export default ButtonPanel
