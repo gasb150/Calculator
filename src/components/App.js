cfb2f9de73bb36b71358a3363ebce1c26b8b31bd
tavo <gasb_150@hotmail.com>
2021-05-11 14:59:49 -0500

    Refactore to class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate';
 
-const App = () => {
-  const [data, setData] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  const handleClick = (buttonName) => {
-    const result = calculate(data, buttonName);
-    setData({ ...result });
-  };
-  const { next, total } = data;
-  return (
-    <div className="ui grid container">
-      <div className="sixteen wide mobile eight wide tablet ten wide computer column">
-        <Display result={(next && next.toString()) || (total && total.toString())} />
-        <ButtonPanel clickHandler={(e) => handleClick(e)} />
+  render() {
+    const handleClick = (buttonName) => {
+      const result = calculate(this.state, buttonName);
+      this.setState({ ...result });
+    };
+    const { next, total } = this.state;
+
+    return (
+      <div className="ui grid container">
+        <div className="sixteen wide mobile eight wide tablet ten wide computer column">
+          <Display result={(next && next.toString()) || (total && total.toString())} />
+          <ButtonPanel clickHandler={(e) => handleClick(e)} />
+        </div>
+        <footer className="footer container center aligned inverted ui header segment">
+          {'Created by '}
+          <a href="https://gasb150.me" className="ui header teal">
+            Gustavo Sanmartin
+          </a>
+        </footer>
       </div>
-      <footer className="footer container center aligned inverted ui header segment">
-        { 'Created by ' }
-        <a href="https://gasb150.me" className="ui header teal">
-          Gustavo Sanmartin
-        </a>
-      </footer>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
