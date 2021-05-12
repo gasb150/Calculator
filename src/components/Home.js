dc601d5ada388e94fdd76538833a80ecf769e6c7
tavo <gasb_150@hotmail.com>
2021-05-12 15:38:32 -0500

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { LoremIpsum } from 'react-lorem-ipsum';
+
+const Home = () => (
+  <>
+    <h3>Welcome to our page!</h3>
+    <LoremIpsum p={2} />
+  </>
+);
+
+export default Home;
